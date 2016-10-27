5b3612b71b7c396af06f98f0db7db67010a5885a
josip-paulik <josip.paulik@outlook.com>
2016-10-27 23:48:05 +0200

    Create project. Create ToDoItem class.

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class ToDoItem
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DateCompleted { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
