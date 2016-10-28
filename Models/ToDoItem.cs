111d9346a1567f6059102d024995ae22efe4dfa4
josip-paulik <josip.paulik@outlook.com>
2016-10-28 11:32:58 +0200

    Add and reference GenericList inside Models projects.

--- Models/ToDoItem.cs
+++ Models/ToDoItem.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GenericList;
+using System;
 
 namespace Models
 {
@@ -13,5 +10,22 @@ namespace Models
         public bool IsCompleted { get; set; }
         public DateTime DateCompleted { get; set; }
         public DateTime DateCreated { get; set; }
+
+        public ToDoItem(string text)
+        {
+            Id = new Guid();
+            Text = text;
+            IsCompleted = false;
+            DateCreated = DateTime.Now;
+        }
+
+        public void MarkAsCompleted()
+        {
+            if(!IsCompleted)
+            {
+                IsCompleted = true;
+                DateCompleted = DateTime.Now;
+            }
+        }
     }
 }
